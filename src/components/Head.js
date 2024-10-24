5604d687ee110cbc7914f26f19685b2c4229441a
Akshay <akshusharmagi@gmail.com>
2024-10-24 12:26:02 +0530

    Error solved

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,110 +1,14 @@
-// import React, { useEffect, useState } from "react";
-// import { useDispatch, useSelector } from "react-redux";
-// import { toggleMenu } from "../utils/appSlice";
-// import { MY_TUBE_LOGO, YOUTUBE_SEARCH_API } from "../utils/constants";
-// import { cacheResults } from "../utils/searchSlice";
-
-// const Head = () => {
-//   const [searchQuery, setSearchQuery] = useState("");
-//   const [suggestions, setSuggestions] = useState([]);
-//   const [showSuggestions, setShowsuggestions] = useState(false);
-//   const searchCache = useSelector((store) => store.search);
-
-//   useEffect(() => {
-//     const timer = setTimeout(() => {
-//       if (searchCache[searchQuery]) {
-//         setSuggestions(searchCache[searchQuery]);
-//       } else {
-//         getSearchSuggestions();
-//       }
-//     }, 200);
-
-//     return () => {
-//       clearTimeout(timer);
-//     };
-//     // eslint-disable-next-line
-//   }, [searchQuery]);
-
-//   const getSearchSuggestions = async () => {
-//     const response = await fetch(YOUTUBE_SEARCH_API + searchQuery);
-//     const json = await response.json();
-//     console.log(json)
-//     const titles = json?.items.map((item) => item?.snippet?.title);
-//     setSuggestions(titles);
-//     dispatch(cacheResults({ [searchQuery]: titles }));
-//   };
-
-//   const dispatch = useDispatch();
-//   const handleToggleMenu = () => {
-//     dispatch(toggleMenu());
-//   };
-
-//   return (
-//     <div className="flex justify-between px-6 py-6 shadow-xl items-center text-center sticky">
-//       <div className="flex gap-6">
-//         <i
-//           onClick={() => handleToggleMenu()}
-//           className="text-2xl cursor-pointer ri-menu-line"
-//         ></i>
-//         <div className="flex">
-//           <img className="h-8" src={MY_TUBE_LOGO} alt="MyTube Logo" />
-//           <h1 className="font-bold pt-1">MyTube</h1>
-//         </div>
-//       </div>
-//       <div className="relative">
-//         <div>
-//           <input
-//             className="h-10 w-[50vw] border border-black p-2 px-6 rounded-2xl"
-//             type="text"
-//             placeholder="Search Here"
-//             value={searchQuery}
-//             onChange={(e) => setSearchQuery(e.target.value)}
-//             onFocus={() => setShowsuggestions(true)}
-//             onBlur={() => setShowsuggestions(false)}
-//           />
-
-//           <button className="absolute w-20 border border-black h-10 right-0 rounded-r-2xl bg-gray-500">
-//             <i className="ri-search-line"></i>
-//           </button>
-//         </div>
-
-//         {showSuggestions && suggestions.length > 0 && (
-//           <div className="absolute bg-white w-[45vw] text-start px-5 py-2 rounded-lg">
-//             <ul>
-//               {suggestions.map((s, index) => (
-//                 <li
-//                   key={index}
-//                   className="py-3 shadow-sm cursor-pointer hover:bg-gray-100  rounded-lg"
-//                 >
-//                   <i className="ri-search-line"></i> {s}
-//                 </li>
-//               ))}
-//             </ul>
-//           </div>
-//         )}
-//       </div>
-//       <div>
-//         <i className="text-3xl ri-user-fill"></i>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Head;
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
 import { MY_TUBE_LOGO, YOUTUBE_SEARCH_API } from "../utils/constants";
 import { cacheResults } from "../utils/searchSlice";
-import { useNavigate } from "react-router-dom";
 
 const Head = () => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
   const [showSuggestions, setShowsuggestions] = useState(false);
   const searchCache = useSelector((store) => store.search);
-  const dispatch = useDispatch();
-  const navigate = useNavigate(); // Hook to navigate programmatically
 
   useEffect(() => {
     const timer = setTimeout(() => {
@@ -124,43 +28,33 @@ const Head = () => {
   const getSearchSuggestions = async () => {
     const response = await fetch(YOUTUBE_SEARCH_API + searchQuery);
     const json = await response.json();
-    console.log(json);
-    
-    // Assuming the API returns video ID along with titles
-    const titlesWithIds = json?.items.map((item) => ({
-      title: item?.snippet?.title,
-      id: item?.id?.videoId, // Extract the video ID
-    }));
-
-    setSuggestions(titlesWithIds);
-    dispatch(cacheResults({ [searchQuery]: titlesWithIds }));
+    console.log(json)
+    const titles = json?.items.map((item) => item?.snippet?.title);
+    setSuggestions(titles);
+    dispatch(cacheResults({ [searchQuery]: titles }));
   };
 
+  const dispatch = useDispatch();
   const handleToggleMenu = () => {
     dispatch(toggleMenu());
   };
 
-  const handleSuggestionClick = (suggestion) => {
-    // Navigate to the watch page using the video ID
-    navigate(`/watch?v=${suggestion.id}`); // Make sure suggestion.id contains the video ID
-  };
-
   return (
-    <div className="flex justify-between px-6 py-6 shadow-xl items-center text-center sticky top-0 bg-white z-50">
+    <div className="flex justify-between px-6 py-6 shadow-xl items-center text-center sticky">
       <div className="flex gap-6">
         <i
           onClick={() => handleToggleMenu()}
           className="text-2xl cursor-pointer ri-menu-line"
         ></i>
-        <div className="flex items-center">
+        <div className="flex">
           <img className="h-8" src={MY_TUBE_LOGO} alt="MyTube Logo" />
           <h1 className="font-bold pt-1">MyTube</h1>
         </div>
       </div>
-      <div className="relative flex-1">
+      <div className="relative">
         <div>
           <input
-            className="h-10 w-full border border-black p-2 px-6 rounded-2xl"
+            className="h-10 w-[50vw] border border-black p-2 px-6 rounded-2xl"
             type="text"
             placeholder="Search Here"
             value={searchQuery}
@@ -175,15 +69,14 @@ const Head = () => {
         </div>
 
         {showSuggestions && suggestions.length > 0 && (
-          <div className="absolute bg-white w-[45vw] text-start px-5 py-2 rounded-lg shadow-lg mt-1">
+          <div className="absolute bg-white w-[45vw] text-start px-5 py-2 rounded-lg">
             <ul>
               {suggestions.map((s, index) => (
                 <li
                   key={index}
-                  className="py-3 shadow-sm cursor-pointer hover:bg-gray-100 rounded-lg"
-                  onClick={() => handleSuggestionClick(s)} // Click handler
+                  className="py-3 shadow-sm cursor-pointer hover:bg-gray-100  rounded-lg"
                 >
-                  <i className="ri-search-line"></i> {s.title}
+                  <i className="ri-search-line"></i> {s}
                 </li>
               ))}
             </ul>
@@ -198,3 +91,4 @@ const Head = () => {
 };
 
 export default Head;
+
